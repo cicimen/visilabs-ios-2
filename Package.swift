a0f77792b618e20b175e34d6a5adda10f888267c
cicimen <egemengulkilik@gmail.com>
2021-05-05 00:45:19 +0300

    VisilabsIOS rename to VisilabsIOSNew

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "VisilabsIOS",
+    name: "VisilabsIOSNew",
     platforms: [
         .iOS(.v10)
     ],
     products: [
         .library(
-            name: "VisilabsIOS",
-            targets: ["VisilabsIOS"]),
+            name: "VisilabsIOSNew",
+            targets: ["VisilabsIOSNew"]),
     ],
     dependencies: [
         // .package(url: /* package url */, from: "1.0.0"),
@@ -20,11 +20,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "VisilabsIOS",
+            name: "VisilabsIOSNew",
             dependencies: []),
         .testTarget(
-            name: "VisilabsIOSTests",
-            dependencies: ["VisilabsIOS"]),
+            name: "VisilabsIOSNewTests",
+            dependencies: ["VisilabsIOSNew"]),
     ],
     swiftLanguageVersions: [.v5]
 )
